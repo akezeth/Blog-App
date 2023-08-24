ead71e4bdc95eb62a96acc01ff35861684cf12b8
akezeth <116703083+akezeth@users.noreply.github.com>
2023-08-24 17:56:35 +0300

    Update create_posts migration file

--- db/migrate/20230824115608_create_posts.rb
+++ db/migrate/20230824115608_create_posts.rb
@@ -4,8 +4,8 @@ class CreatePosts < ActiveRecord::Migration[7.0]
       t.references :author, null: false, foreign_key: { to_table: :users }
       t.string :title
       t.string :text
-      t.integer :commentscounter
-      t.integer :likescounter
+      t.integer :comments_counter
+      t.integer :likes_counter
 
       t.timestamps
     end
