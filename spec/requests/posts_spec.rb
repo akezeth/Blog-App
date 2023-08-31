39b4509cb45d8b990ed95c29e70007df38070a68
akezeth <116703083+akezeth@users.noreply.github.com>
2023-08-31 09:38:47 +0300

    Add test for controllers

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,40 @@
 require 'rails_helper'
 
-RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'PostsController', type: :request do
+  describe 'GET /users/:user_id/posts/index' do
+    let(:user) { User.create(name: 'test user') }
+
+    before(:example) { get user_posts_path(user) }
+
+    it 'displays a list of posts' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the correct template' do
+      expect(response).to render_template('index')
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      expect(response.body).to include('Here is a list of posts for a given user')
+    end
+  end
+
+  describe 'GET /users/:user_id/posts/:id' do
+    let(:user) { User.create(name: 'test user') }
+    let(:post) { user.posts.create(title: 'Test Post') }
+
+    before(:example) { get user_post_path(user, post) }
+
+    it 'returns a successful response' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the correct template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      expect(response.body).to include('Here is a specific post for a given user')
+    end
   end
 end
